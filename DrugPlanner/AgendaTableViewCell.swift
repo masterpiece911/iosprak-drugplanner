94e94c44ffae19d9d0c84d250b25f81751f412dd
Noyan Tillman Sahin <sahin@cip.ifi.lmu.de>
2017-06-07 11:35:44 +0200

    Ordnerstruktur erstellt

---
+++ DrugPlanner/AgendaTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AgendaTableViewCell.swift
+//  DrugPlanner
+//
+//  Created by admin on 07.06.17.
+//  Copyright © 2017 Gruppe 9. All rights reserved.
+//
+
+import UIKit
+
+class AgendaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
