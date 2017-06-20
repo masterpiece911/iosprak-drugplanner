d67aaece337d7942d348c9cc0bab64f6cd1e3871
ioana-raluca pica <ioana-pica@dhcp-10-180-25-125.dynamic.eduroam.mwn.de>
2017-06-20 08:32:22 +0200

    Custom Cells zur Inventory View hinzugefügt

---
+++ DrugPlanner/HistoryContentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HistoryContentTableViewCell.swift
+//  DrugPlanner
+//
+//  Created by ioana-raluca pica on 20.06.17.
+//  Copyright © 2017 Gruppe 9. All rights reserved.
+//
+
+import UIKit
+
+class HistoryContentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
