5e872051ddb424885162cd7cf68a41372af2da0b
ioana-raluca pica <ioana-pica@dhcp-10-180-19-249.dynamic.eduroam.mwn.de>
2017-06-21 12:04:36 +0200

    bug fixes für Type Picker

--- DrugPlanner/ChooseTypeTableViewController.swift
+++ DrugPlanner/ChooseTypeTableViewController.swift
@@ -28,7 +28,7 @@ class ChooseTypeTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+                
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
