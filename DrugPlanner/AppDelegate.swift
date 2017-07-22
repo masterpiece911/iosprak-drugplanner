4e00e4dd30cba18e1b130ebb96168eebc954d604
elena.karypidou <e.karypidou@campus.lmu.de>
2017-07-22 15:39:38 +0200

    ?

--- DrugPlanner/AppDelegate.swift
+++ DrugPlanner/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         FirebaseApp.configure()
-        Database.database().isPersistenceEnabled = true;
+        Database.database().isPersistenceEnabled = false;
         ref = Database.database().reference()
 
         
