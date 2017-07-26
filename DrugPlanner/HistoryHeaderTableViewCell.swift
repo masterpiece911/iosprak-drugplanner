4e7750a95dc4282d65a336e6aab04c22807f87fa
ioana-raluca pica <ioana-pica@dhcp-10-180-27-39.dynamic.eduroam.mwn.de>
2017-07-26 15:14:42 +0200

    AddHistoryItemView ENDLICH hinzugefügt !! Cancel button funktioniert noch nicht...

---
+++ DrugPlanner/HistoryHeaderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HistoryHeaderTableViewCell.swift
+//  DrugPlanner
+//
+//  Created by ioana-raluca pica on 26.07.17.
+//  Copyright © 2017 Gruppe 9. All rights reserved.
+//
+
+import UIKit
+
+class HistoryHeaderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
