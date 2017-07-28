261e5d8fd0b586055c211412325745c628cb28f1
The Masterpiece <the.masterpiece@outlook.com>
2017-07-28 04:47:09 +0200

    InventarTableViewController erzeugt HistoryItems
    Der InventarTableViewController erzeugt HistoryItems, indem man ein InventoryItem zur Seite schiebt, Take antippt und eine gültige Dosis in das erscheinende Textfeld eingibt.

--- DrugPlanner/InventoryViewController.swift
+++ DrugPlanner/InventoryViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
-class InventoryViewController: UITableViewController {
+class InventoryViewController: UITableViewController, UITextFieldDelegate {
     
     var items = [InventoryItem]()
     
     var observer : Any?
-
+    
+    var alertController : UIAlertController?
+    
+    var takenIndex : Int?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -63,6 +66,25 @@ class InventoryViewController: UITableViewController {
 
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let markTaken = UITableViewRowAction(style: .normal, title: "Take", handler: {
+            action, index in
+            print("\(self.items[index.row].InventoryItemName) as taken")
+            self.takenIndex = index.row
+            self.drugTakenTapped(index.row)
+        })
+        
+        markTaken.backgroundColor = UIColor(colorLiteralRed: 0.15294117647058825
+, green: 0.6823529411764706, blue: 0.3764705882352941, alpha: 1)
+        
+        return [markTaken]
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
     }
     
     
@@ -145,5 +167,81 @@ class InventoryViewController: UITableViewController {
         self.tableView.reloadData()
         
     }
+    
+    func drugTakenTapped (_ index: Int) {
+        
+        self.alertController = UIAlertController(title: "Confirm that you took \(items[index].InventoryItemName)?", message: "This will be noted in your history.", preferredStyle: .alert)
+
+        alertController!.addTextField(configurationHandler: { (textField : UITextField!) -> Void in
+            
+            textField.placeholder = "Enter your intake dose"
+            textField.keyboardType = .numberPad
+            textField.delegate = self
+            
+        })
+        
+        let alertConfirmAction = UIAlertAction(title: "Confirm", style: .default, handler: {
+            action in
+            
+            let takenItem = self.items[index]
+            let takenAmount = Int((self.alertController!.textFields?[0].text!)!)!
+            takenItem.InventoryItemAmount -= takenAmount
+            let now = Date()
+            
+            Inventory.instance.edit(inventory: takenItem)
+            History.instance.add(historyItem: HistoryItem.init(withInventory: takenItem, withAmount: takenAmount, atDate: now, withNotes: "", usingKey: "tmp"))
+        })
+        
+        alertConfirmAction.isEnabled = false
+        
+        let alertCancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        
+        alertController!.addAction(alertConfirmAction)
+        alertController!.addAction(alertCancelAction)
+        
+        present(alertController!, animated: true, completion: nil)
+        
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let previousInput = textField.text!
+        
+        if string == "" && previousInput.substring(to: previousInput.index(before: previousInput.endIndex)) == "" {
+            self.alertController!.actions[0].isEnabled = false
+            self.alertController!.message = "An intake dose is required."
+            return true
+        } else {
+            var newInput : String
+            if string == "" {
+                newInput = previousInput.substring(to: previousInput.index(before: previousInput.endIndex))
+            } else {
+                newInput = previousInput.appending(string)
+            }
+            
+            if let intake = Int(newInput) {
+                
+                if intake <= self.items[self.takenIndex!].InventoryItemAmount {
+                    self.alertController!.actions[0].isEnabled = true
+                    self.alertController!.message = "This will be noted in your history."
+                } else {
+                    self.alertController!.actions[0].isEnabled = false
+                    self.alertController!.message = "Your intake dose can not be higher than your remaining doses."
+                }
+                
+            }
+            
+            
+            return string == "" || Int(string) != nil
+
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
     
 }
